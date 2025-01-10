9214ff8a464d9c04874ade5a85ae554a8d263762
Jason Lee <jasonmlee.career@gmail.com>
2025-01-09 21:01:58 -0800

    updated test files

--- tests/test_pycounter.py
+++ tests/test_pycounter.py
@@ -1,1 +1,4 @@
-from pycounter import pycounter
+from pycounter.pycounter import count_words
+from pycounter.plotting import plot_words
+counts = count_words("zen.txt")
+fig = plot_words(counts, 10)
